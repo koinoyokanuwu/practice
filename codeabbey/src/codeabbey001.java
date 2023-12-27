ce45eb7e0c1857f111faed2dcc9978ba54cd95a6
koinoyokanuwu <koinoyokanuwu@gmail.com>
2023-12-27 16:45:29 -0500

    codeabbey

---
+++ codeabbey/src/codeabbey001.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class codeabbey001 {
+    public static void main(String[] args) {
+        //Datos
+        float dato1= 0;
+        float dato2 = 0;
+        float producto = 0;
+        //Lectura
+        Scanner usuario = new Scanner(System.in);
+        //Recolección
+        System.out.println("Digite el primer número:");
+        dato1 = usuario.nextFloat();
+        System.out.println("Digite el segundo número");
+        dato2 = usuario.nextFloat();
+        //Cálculo
+        producto = dato1 + dato2;
+        //Respuesta
+        System.out.println("El valor de la suma es: "+ producto);
+    }
+}
